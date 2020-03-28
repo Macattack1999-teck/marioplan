eaa6dffc8e2b5c2528c59131686b35595bf99841
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-28 13:08:13 -0600

    Created the functionality of the Sign in and Sign up components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Dashboard from './components/dashboard/Dashboard';
-import ProjectDetails from './components/projects/ProjectDetails'
 
 import Navbar from './components/layout/Navbar'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard} />
           <Route path="/project/:id" component={ProjectDetails} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signup" component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>
