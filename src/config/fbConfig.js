29774c7a44ac69b7656a896e8a2188e63a774884
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-28 14:26:33 -0600

    set my application to use firebase features

---
+++ src/config/fbConfig.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC-OSN40btwzpXCuxW31NVY453Uw3l7h-g",
+  authDomain: "react-redux-firebase-pra-49a25.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-pra-49a25.firebaseio.com",
+  projectId: "react-redux-firebase-pra-49a25",
+  storageBucket: "react-redux-firebase-pra-49a25.appspot.com",
+  messagingSenderId: "144110325154",
+  appId: "1:144110325154:web:6a3880678fe2b18ba876fb",
+  measurementId: "G-GZT6N910R4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
