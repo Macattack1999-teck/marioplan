3f135551922a7bd92b2223097f5b3e9d54159d09
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-28 20:17:45 -0600

    Populating user profile data when they create a post

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -17,6 +17,7 @@ class CreateProject extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     this.props.createProject(this.state)
+    this.props.history.push('/');
   }
 
   render() {
--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -5,7 +5,7 @@ const ProjectSummary = ({project}) => {
     <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title">{project.title}</span>
-          <p>Posted by Michael Cortez</p>
+          <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
           <p className="grey-text">27th March, 12pm</p>
         </div>
     </div>
