0096c00135d61e14f190f8e6a2200dc7f222cc0d
Michale-Cortez <CortezDevelopement@gmail.com>
2020-03-28 20:02:08 -0600

    now pulling in user profile data and rendering it to the dom

--- src/index.js
+++ src/index.js
@@ -1,49 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware, compose } from 'redux'
-import rootReducer from './store/reducers/rootReducer'
-import { Provider, useSelector } from 'react-redux'
-import thunk from 'redux-thunk'
-import { createFirestoreInstance, getFirestore, reduxFirestore } from 'redux-firestore'
-import { ReactReduxFirebaseProvider, getFirebase, isLoaded } from 'react-redux-firebase'
-import fbConfig from './config/fbConfig'
-import firebase from 'firebase/app'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./store/reducers/rootReducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {
+  createFirestoreInstance,
+  getFirestore,
+  reduxFirestore
+} from "redux-firestore";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+import firebase from "firebase/app";
+
+//for render on auth ready
+import { useSelector  } from 'react-redux'
+import { isLoaded  } from 'react-redux-firebase';
+
+
+
 
 const store = createStore(
-    rootReducer,
-    compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-        reduxFirestore(firebase, fbConfig)
-    )
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(firebase, fbConfig)
+  )
 );
+
+const profileSpecificProps = {
+  userProfile: 'users',
+  useFirestoreForProfile: true,
+  enableRedirectHandling: false,
+  resetBeforeLogin: false
+}
+
 
 const rrfProps = {
   firebase,
   config: fbConfig,
+  config: profileSpecificProps,
   dispatch: store.dispatch,
-  createFirestoreInstance,
-  userProfile: 'users', // where profiles are stored in database
-  presence: 'presence', // where list of online users is stored in database
-  sessions: 'sessions'
-}
+  createFirestoreInstance
+};
 
 function AuthIsLoaded({ children }) {
   const auth = useSelector(state => state.firebase.auth)
-  if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+  if (!isLoaded(auth)) return <div className="center"> <p>Loading Mario Plan...</p></div>;
       return children
 }
+
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
       <AuthIsLoaded>
-        <App />
+      <App />
       </AuthIsLoaded>
+      
     </ReactReduxFirebaseProvider>
-  </Provider>, document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
-serviceWorker.unregister(); 
+serviceWorker.unregister();
